307d7779d50ffeb736ad7c99efb75a66deb9a331
Arpan Pramanik <pramanikarpan089@gmail.com>
2026-08-20 20:53:19 +0530

    feat: configure Next.js with reactStrictMode and unoptimized image settings

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,36 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  agentRules: false,
   images: {
     unoptimized: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'arpanpramanik.vercel.app',
-          },
-        ],
-        destination: 'https://arpanpramanik.dev/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.arpanpramanik.dev',
-          },
-        ],
-        destination: 'https://arpanpramanik.dev/:path*',
-        permanent: true,
-      },
-    ];
   },
 };
 
 export default nextConfig;
+
